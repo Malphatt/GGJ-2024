ae7d0adaf08a06a2da4f32a1a97e1f82b3b8db54
ThatGuyDed <44003663+ThatGuyDed@users.noreply.github.com>
2024-01-27 17:03:00 +0000

    new map stuff

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -23,6 +23,6 @@ public class PlayerManager : MonoBehaviour
 
     void CreateController()
     {
-        PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "Player"), new Vector3(49,-11,144), Quaternion.identity);
+        PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "Player"), new Vector3(-8,54,89), Quaternion.identity);
     }
 }
