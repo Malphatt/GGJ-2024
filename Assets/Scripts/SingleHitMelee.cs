896f634645ff02edff00ebd3229f2a59aadfe256
ThatGuyDed <44003663+ThatGuyDed@users.noreply.github.com>
2024-01-27 14:30:41 +0000

    anim fix

--- Assets/Scripts/SingleHitMelee.cs
+++ Assets/Scripts/SingleHitMelee.cs
@@ -8,6 +8,12 @@ public class SingleHitMelee : Weapon
     public override void Use()
     {
         Debug.Log("Punch");
-        animator.Play("Punch");
+        animator.SetFloat("Punch", 1);
+        Invoke("UnPunch", 0.3f);
+    }
+
+    void UnPunch()
+    {
+        animator.SetFloat("Punch", -1);
     }
 }
