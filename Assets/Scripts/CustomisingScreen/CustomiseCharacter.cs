f34fb082cd9de0196d0a2eeb02c5aa821813c47a
Michal Redzko <117772544+RedCore0@users.noreply.github.com>
2024-01-28 09:56:03 +0000

    added wearable items to the player prefab

--- Assets/Scripts/CustomisingScreen/CustomiseCharacter.cs
+++ Assets/Scripts/CustomisingScreen/CustomiseCharacter.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CustomiseCharacter : MonoBehaviour
 {
@@ -9,8 +10,10 @@ public class CustomiseCharacter : MonoBehaviour
     int Type;
     bool Equipped = false;
     GameObject EqupippedObj;
+    Image ThisImage;
     private void Start()
     {
+        ThisImage = GetComponent<Image>();
         Tin = transform.parent.GetComponent<CustomiseManager>().Tin;
         Type = transform.GetComponent<WearableType>().Type;
         placement = transform.parent.GetComponent<CustomiseManager>().wearablePlacement[Type];
@@ -19,12 +22,14 @@ public class CustomiseCharacter : MonoBehaviour
     {
         if (!Equipped)
         {
+            ThisImage.color = Color.green;
             Quaternion rot = Quaternion.Euler(-90f, 0f, 0f);
             EqupippedObj = Instantiate(wearable, placement.position, rot, Tin);
             Equipped = true;
         }
         else
         {
+            ThisImage.color = Color.white;
             Destroy(EqupippedObj);
             Equipped = false;
         }
